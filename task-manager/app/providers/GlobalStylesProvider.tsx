ff03c6cbfd20efbdfd28eefc5509c2af9ffc1093
Michael Florentio <maikelflorentio@gmail.com>
2023-12-09 19:52:54 +0700

    initial commit

---
+++ task-manager/app/providers/GlobalStylesProvider.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React, { Children } from 'react'
+import styled from 'styled-components';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+function GlobalStylesProvider({children} : Props ) {
+  return (
+    <GlobalStyles>{children}</GlobalStyles>
+  )
+}
+
+const GlobalStyles = styled.div`
+    background-color: red;
+
+`
+
+export default GlobalStylesProvider
