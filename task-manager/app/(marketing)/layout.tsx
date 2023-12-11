60cfb1eb68e1fd2b7915dc5d9c7089a5e18ee06b
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-12-11 23:43:59 +0700

    feat dashboard , clerks authentication system

---
+++ task-manager/app/(marketing)/layout.tsx
@@ -0,0 +1,20 @@
+import { Navbar } from './_components/Navbar'
+import { Footer } from './_components/Footer'
+
+const layout = ({
+    children
+} : { 
+    children: React.reactNode;
+}) => {
+  return (
+    <div className="h-full bg-slate-100">
+        <Navbar />
+        <main className="pt-40 pb-20 bg-slate-100">
+            {children}
+        </main>
+        <Footer />
+    </div>
+  )
+}
+
+export default layout
---
+++ task-manager/app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,14 @@
+
+const clerkLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="flex h-full items-center justify-center">
+            {children}
+        </div>
+    )
+}
+
+export default clerkLayout
