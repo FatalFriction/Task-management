0176f89b24793cfa685bcf5236763c90ff6ba74c
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-12-15 02:29:42 +0700

    feat board ui

--- task-manager/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ task-manager/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,13 @@
 import { create } from "@/actions/create-Board"
 import { Button } from "@/components/ui/button"
+import { db } from "@/lib/db";
+import { Board } from "./board";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany();
 
     return (
-        <div>
+        <div className="flex flex-col space-y-4">
             <form action={create}>
                 <input
                     id="title"
@@ -17,6 +20,11 @@ const OrganizationIdPage = () => {
                     Submit
                 </Button>
             </form>
+            <div className="space-y-2">
+                {boards.map((board) => (
+                    <Board key={board.id} title={board.title} id={board.id}/>
+                ))}
+            </div>
         </div>
     )
 }
