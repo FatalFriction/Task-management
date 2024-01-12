bf76107d461cbeaa6497c5b58b071f9a65622691
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-01-12 18:22:33 +0700

    feat types

--- task-manager/types.ts
+++ task-manager/types.ts
@@ -1,6 +1,8 @@
-import { Card, List } from "@prisma/client";
+import { Card, CardUrl, List } from "@prisma/client";
 
 
 export type ListWithCards = List & { cards: Card[] }
 
 export type CardWithList = Card & { list: List }
+
+export type CardUrlWithCard = CardUrl & { card: Card }
