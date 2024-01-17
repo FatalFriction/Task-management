823eb3860bb8acf288764c661c7c3d999cd44fba
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-01-18 04:23:02 +0700

    security: name refactor

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -5,15 +5,15 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  // Add your API key here
-  apiKey: process.env.FBASE_API_KEY,
-  authDomain: process.env.FBASE_AUTH_DOMAIN,
-  projectId: process.env.FBASE_PROJECT_ID,
-  storageBucket: process.env.FBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FBASE_APP_ID,
-  measurementId: process.env.FBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
