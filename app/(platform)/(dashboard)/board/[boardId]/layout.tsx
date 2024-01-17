bb62bdaf85aa6d9f75a0f81f17cc689cd5745d49
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-01-18 02:58:46 +0700

    improve code readability

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -5,75 +5,71 @@ import { BoardNavbar } from "./_components/board-navbar";
 import Image from "next/image";
 
 export async function generateMetadata({
-    params,
-} : { 
-    params: { boardId: string }
+  params,
+}: {
+  params: { boardId: string };
 }) {
-    const { orgId } = auth()
-
-    if(!orgId){
-        return {
-            title: "Board"
-        }
-    }
-
-    const board = await db.board.findUnique({
-        where: {
-            id: params.boardId,
-            orgId,
-        }
-    })
-
+  const { orgId } = auth();
+  if (!orgId) {
     return {
-        title: board?.title || "Board"
-    }
+      title: "Board",
+    };
+  }
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+  return {
+    title: board?.title || "Board",
+  };
 }
 
 const BoardIdLayout = async ({
-    children,
-    params,
-} : {
-    children: React.ReactNode,
-    params: { boardId: string }
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
 }) => {
-    const { orgId } = auth()
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect("/select-org");
+    return null; // Return early if redirecting
+  }
 
-    if(!orgId) {
-        redirect("/select-org")
-    }
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
 
-    const board = await db.board.findUnique({
-        where: {
-            id: params.boardId,
-            orgId,
-        }
-    })
+  if (!board) {
+    notFound();
+    return null; // Return early if board not found
+  }
 
-    if(!board) {
-        notFound();
-    }
-
-    return (
-        <div className="relative h-full">
-            <div>
-                <Image
-                    src={board.imageFullUrl}
-                    alt="board images"
-                    quality={50}
-                    fill
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    loading="eager"
-                    className="relative"
-                    style={{ objectFit: 'cover'}}
-                />
-                <BoardNavbar data={board}/>
-                <div className="absolute inset-0 bg-black/10"/>
-                <main className="relative pt-28 h-full">
-                    {children}
-                </main>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="relative h-full">
+      <>
+        <Image
+          src={board.imageFullUrl}
+          alt="board images"
+          quality={100} // Use higher quality image
+          fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          loading="eager"
+          className="relative"
+          style={{ objectFit: "cover" }}
+        />
+        <BoardNavbar data={board} />
+        <div className="absolute inset-0 bg-black/10" />
+        <main className="relative pt-28 h-full">{children}</main>
+      </>
+    </div>
+  );
+};
 
 export default BoardIdLayout;
