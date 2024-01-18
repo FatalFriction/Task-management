fcf73ddb51dc646ac3c923682e58e630cedd929f
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-01-19 03:52:30 +0700

    fix: added natural size of image to contain image size

--- components/modals/card-modals/_components-images/image-fullscreen.tsx
+++ components/modals/card-modals/_components-images/image-fullscreen.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 interface ExpandedImageModalProps {
     imageUrl: string;
@@ -6,11 +7,31 @@ interface ExpandedImageModalProps {
   }
   
 export const ExpandedImageModal: React.FC<ExpandedImageModalProps> = ({ imageUrl, onClose }) => {
+
+    const [imageWidth, setImageWidth] = useState(0);
+    const [imageHeight, setImageHeight] = useState(0);
+
+    const handleImageLoad = (event: React.SyntheticEvent<HTMLImageElement>) => {
+      const img = event.currentTarget;
+      setImageWidth(img.naturalWidth);
+      setImageHeight(img.naturalHeight);
+    };
+
     return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="absolute inset-0 bg-black opacity-75" onClick={onClose}></div>
-        <div className="z-10 max-w-screen-[2400px] max-h-full overflow-auto">
-          <Image src={imageUrl} alt="Expanded Image" width={1080} height={768} objectFit="contain"  objectPosition="50% 50%"/>
+      <div className="fixed inset-0 z-50 flex flex-wrap items-center justify-center p-4">
+        <div className="absolute inset-0 bg-black opacity-75" onClick={onClose}/>
+        <div className="z-10 overflow-auto">
+          <Image
+            src={imageUrl}
+            alt="Expanded Image"
+            sizes="(max-width: 768px,max-height: 768px) 40vw"
+            onLoad={handleImageLoad}
+            layout="responsive"
+            objectFit="contain"
+            objectPosition="50% 50%"
+            width={imageWidth}
+            height={imageHeight}
+          />
         </div>
       </div>
     );
